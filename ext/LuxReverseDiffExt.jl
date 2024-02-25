ae90a8ef6763673c5db9219229aaa7d36e1fcd2c
Avik Pal <avikpal@mit.edu>
2024-02-25 18:02:06 -0500

    Remove ComponentArrays type-piracies

--- ext/LuxReverseDiffExt.jl
+++
@@ -1,8 +0,0 @@
-module LuxReverseDiffExt
-
-using Lux, ReverseDiff
-
-Lux.__value(x::AbstractArray{<:ReverseDiff.TrackedReal}) = ReverseDiff.value.(x)
-Lux.__value(x::ReverseDiff.TrackedArray) = ReverseDiff.value(x)
-
-end
